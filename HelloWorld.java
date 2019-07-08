f0c3c1923ff94178e34130a83372cec3a7ee2d26
Ashu Jain <ashu.jain01@gmail.com>
2019-07-08 04:14:46 +0000

    Added a new print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ package devops.git
 //this Code will print the Hello World message
 public static void main( String [] args)
 {
-System.out.println("Hello World")
+System.out.println("Hello World");
+System.out.println("Hello Git");
 }
 
